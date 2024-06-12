08d7acb36b9b303c5cfc5aa46b8489b0971d472b
lucas <lucasprotch@gmail.com>
2024-06-12 21:39:17 +0200

    [+] add blank project

---
+++ Source/final/finalGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "finalGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FINAL_API AfinalGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
